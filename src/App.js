04f3ee049a63a2be3cca1839ce155e8c5c8ac847
Puru <puru@webintensive.com>
2024-12-26 12:55:57 +0530

    Started a new portfolio webiste

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import { Fragment } from 'react';
+import { motion } from 'framer-motion';
+
+
+function App() {
+  return (
+    //   <motion.div
+    //     initial = {{ opacity : 0 , scale : 0.1}}
+    //     whileInView={{ opacity : 1 , scale : 1}}
+    //     transition={{duration : 0.5}}
+    //     className='h-[200px] w-[500px] border-[2px] border-[black] m-auto overflow-hidden rounded-2xl flex justify-start items-center p-[10px] mt-[120vh]'>
+    //     <motion.div
+    //       className='h-[100px] w-[100px] rounded-[10px] bg-slate-500 relative'
+    //       initial={{ opacity: 0, x: "-110px" }}
+    //       whileInView={{ opacity: 1, rotate: 360, x: "0px" }}
+    //       transition={{ duration: 1 }}
+    //     >
+    //     </motion.div>
+    //   </motion.div>
+    <div className='h-[100%] w-[100%] bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]'>
+
+    </div>
+  );
+}
+
+export default App;
