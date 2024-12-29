dce9bf1abb928a356e8585408ca889048653db3d
Puru <puru@webintensive.com>
2024-12-29 15:29:56 +0530

    Fixing issues

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,40 +1,50 @@
-import puru from "./puru.png" ; 
-import cpp from "./cpp.png" ; 
-import html from "./html.png" ; 
-import javaScript from "./javaScript.png" ; 
-import mongo from "./mongoDB.png" ; 
-import express from "./express.png" ; 
-import reactJs from "./react.png" ; 
-import node from "./node.png" ; 
-import tailwind from "./tailwind.png" ; 
-import solidity from "./solidity.svg" ; 
-import belzabar from "./belzabar.jpeg" ; 
-import boiam from "./boiam.jpeg" ; 
-import p1 from "./project1.png" ;
-import p2 from "./project2.png" ;
-import p3 from "./project3.png" ;
-import material from "./materialUI.png" ;
-import metamask from "./metamask.png" ;
-import ether from "./ether.svg" ;
-import boot from "./bootstrap.png" ;
-import framer from "./framer.png" ;
-import css from "./css.png" ;
-import p4 from "./projectF.png"
+import puru from "./puru.png";
+import cpp from "./cpp.png";
+import html from "./html.png";
+import javaScript from "./javaScript.png";
+import mongo from "./mongoDB.png";
+import express from "./express.png";
+import reactJs from "./react.png";
+import node from "./node.png";
+import tailwind from "./tailwind.png";
+import solidity from "./solidity.svg";
+import belzabar from "./belzabar.jpeg";
+import boiam from "./boiam.jpeg";
+import p1 from "./project1.png";
+import p2 from "./project2.png";
+import p3 from "./project3.png";
+import material from "./materialUI.png";
+import metamask from "./metamask.png";
+import ether from "./ether.svg";
+import boot from "./bootstrap.png";
+import framer from "./framer.png";
+import css from "./css.png";
+import p4 from "./projectF.png";
 
-export default {
-    puru , 
-    cpp , 
-    html , 
-    javaScript , 
-    mongo , 
-    express , 
-    reactJs , 
-    node , 
-    tailwind , 
-    solidity , 
-    belzabar , 
-    boiam , 
-    material,
-    metamask, framer , css , 
-    p1 , p2 , p3, p4, ether , boot
-}
+// Assign the object to a variable
+const images = {
+  puru,
+  cpp,
+  html,
+  javaScript,
+  mongo,
+  express,
+  reactJs,
+  node,
+  tailwind,
+  solidity,
+  belzabar,
+  boiam,
+  material,
+  metamask,
+  framer,
+  css,
+  p1,
+  p2,
+  p3,
+  p4,
+  ether,
+  boot,
+};
+
+export default images;
